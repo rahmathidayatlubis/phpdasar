6f61761f343e2ffa8b63a4e128126263fbb674ce
rahmathidayatlubis <rahmattlubis86@gmail.com>
2023-04-12 21:30:25 +0700

    menambah jquery

--- index.php
+++ index.php
@@ -126,7 +126,7 @@ if (isset($_POST["cari"])) {
 
 
 
-
+    <script src="js/jquery-3.6.4.min.js"></script>
     <script src="js/script.js"></script>
 </body>
 
--- js/script.js
+++ js/script.js
@@ -1,26 +1,10 @@
-// ambil elemen yang dibutuhkan
+$(document).ready(function () {
+  // sembunyikan tombol search
+  $('#tombol-cari').hide();
 
-const keyword = document.getElementById('keyword');
-const tombolCarai = document.getElementById('tombol-cari');
-const tabelMhs = document.getElementById('tabel-mhs');
-
-// tambahkan event ketika keyword diketik
-keyword.addEventListener('keyup', function () {
-  
-  // buat object ajax
-  let xhr = new XMLHttpRequest();
-
-  // cek kesiapan ajax
-  xhr.onreadystatechange = function () {
-    if( xhr.readyState == 4 && xhr.status == 200 ){
-      tabelMhs.innerHTML = xhr.responseText;
-
-    }
-  }
-
-  // eksekusi ajax
-  xhr.open('GET', 'ajax/mahasiswa.php?keyword='+ keyword.value, true);
-  xhr.send();
-
+  // even ketika disearch
+  $('#keyword').on('keyup', function (params) {
+    $('.tabel-mhs').load('ajax/mahasiswa.php?keyword=' + $('#keyword').val());
+  });
 
 });
